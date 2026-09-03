ed1ed16e16ca9d92dbd336771382750af492a417
comardom <Comardom@outlook.com>
2026-09-03 21:22:13 +0800

    0.7.4

---
+++ src/data/web.ts
@@ -0,0 +1,12 @@
+export const webPage = {
+    seo: {
+        title: '网页部 | 河南大学网站工作室',
+        description: '河南大学网站工作室网页部。',
+    },
+    verses: [
+        { id: 'Oramus', title: 'Oramus iter per astra' },
+        { id: 'Lux', title: 'Lux tua nos dirigat' },
+        { id: 'Terra', title: 'Ubi Promissa Terra' },
+        { id: 'Spes', title: 'Ibi erit spes' },
+    ],
+} as const
